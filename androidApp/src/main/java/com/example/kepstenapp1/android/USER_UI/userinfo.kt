a99145fbe88ae69f852b2440107a37f5d69a9cdc
mayankjonwal02 <jonwal.1@iitj.ac.in>
2023-04-04 12:06:53 +0530

    User Info page added

--- androidApp/src/main/java/com/example/kepstenapp1/android/USER_UI/userinfo.kt
+++ androidApp/src/main/java/com/example/kepstenapp1/android/USER_UI/userinfo.kt
@@ -5,13 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -36,25 +38,27 @@ fun userinfo() {
             .background(Color.White)
     )
     {
+
         Column(
             modifier = Modifier
                 .fillMaxHeight()
                 .verticalScroll(rememberScrollState())
+                .padding(top= 60.dp,bottom = 80.dp)
         ) {
-            Spacer(modifier = Modifier.height(30.dp))
-            Text(
-                text = "USER INFO",
-                modifier = Modifier.fillMaxWidth(),
-                style = TextStyle(
-                    color = Color.Black,
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.ExtraBold,
-                    fontStyle = FontStyle.Normal,
-                    fontFamily = FontFamily.Cursive,
-                    textAlign = TextAlign.Center
-                )
-            )
-            Spacer(modifier = Modifier.height(100.dp))
+            //Spacer(modifier = Modifier.height(50.dp))
+//            Text(
+//                text = "USER INFO",
+//                modifier = Modifier.fillMaxWidth(),
+//                style = TextStyle(
+//                    color = Color.Black,
+//                    fontSize = 40.sp,
+//                    fontWeight = FontWeight.ExtraBold,
+//                    fontStyle = FontStyle.Normal,
+//                    fontFamily = FontFamily.Cursive,
+//                    textAlign = TextAlign.Center
+//                )
+//            )
+            Spacer(modifier = Modifier.height(80.dp))
             Card(
                 modifier = Modifier
                     .padding(16.dp)
@@ -155,7 +159,8 @@ fun userinfo() {
                     keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
                     maxLines = 1,
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 8.dp)
                 )
             }
@@ -263,6 +268,43 @@ fun userinfo() {
                     )
                 )
             }
+        }
+
+        // update button
+        Text(
+            text = "USER INFO",
+            modifier = Modifier
+
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .padding(50.dp)
+                .background(Color.White)
+                ,
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontStyle = FontStyle.Normal,
+                fontFamily = FontFamily.Cursive,
+                textAlign = TextAlign.Center
+            )
+        )
+
+
+        var text by remember { mutableStateOf("Update") }
+
+        OutlinedButton(
+            onClick = { text = "Save" },
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.padding(16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(
+                text = text,
+                color = Color.Red,
+
+                style = TextStyle(fontSize = 20.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold, fontStyle = FontStyle.Normal)
+            )
         }
 
 
